1628023adc0c115c33d3d61424759d6682b968bb
Karim Bassem <kimobisso@gmail.com>
2025-04-12 16:01:14 +0200

    feat: Implement patient update and delete functionality; enhance patient details view

--- doctor/urls.py
+++ doctor/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('add-patient/', views.add_patient, name='add_patient'),
     path('show-patients/', views.show_patients, name='show_patients'),
     path('show-patient-details/', views.show_patient_details, name='show_patient_details'),
+    path('update-patient/', views.update_patient, name='update_patient'),
+    path('delete-patient/', views.delete_patient, name='delete_patient'),
 ]
--- doctor/views.py
+++ doctor/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from accounts.decorators import doctor_required
 from datetime import datetime, date, timedelta
+from django.urls import reverse
 
 @login_required
 @doctor_required
@@ -97,9 +98,11 @@ def add_patient(request):
         try:
             new_patient.save()
             messages.success(request, "Patient Added successfully!")
-            return redirect("doctor_dashboard")
-        except:
-            messages.error(request, "Something went wrong while saving the patient data")
+            patient_id = new_patient.id
+            url = reverse('show_patient_details') + f'?patient_id={patient_id}'
+            return redirect(url)
+        except Exception as e:
+            messages.error(request, f"Something went wrong while saving the new patient data: {str(e)}")
             return redirect("doctor_dashboard")
     context={
         "user_data": request.user
@@ -128,6 +131,61 @@ def show_patient_details(request):
     
     context = {
         "user_data": request.user,
-        "patients": [patient]  # Keep as list for compatibility with existing template
+        "patients": [patient] # Keep as list for compatibility with existing template
     }
     return render(request, "show_patient_details.html", context)
+
+@login_required
+@doctor_required
+def update_patient(request):
+    patient_id = request.GET.get('patient_id')
+    doctor_profile = get_object_or_404(DoctorProfile, user=request.user)
+    patient = get_object_or_404(Patients, doctor=doctor_profile, id=patient_id)
+
+    if request.method == 'GET': 
+        context = {
+            "user_data": request.user,
+            "patients": [patient]
+        }
+        return render(request, "edit_patient.html", context)
+
+    name = request.POST.get('name')
+    phone_number = request.POST.get('phone_number')
+    gender = request.POST.get('gender')
+    date_of_birth = request.POST.get('date_of_birth')
+
+    patient.name = name
+    patient.phone_number = phone_number
+    patient.gender = gender
+    patient.date_of_birth = date_of_birth
+
+    try:
+        patient.save()
+        messages.success(request, "Patient Updated successfully!")
+        url = reverse('show_patient_details') + f'?patient_id={patient_id}'
+        return redirect(url)
+    except Exception as e:
+        messages.error(request, f"Something went wrong while saving the new patient data: {str(e)}")
+        url = reverse('update_patient') + f'?patient_id={patient_id}'
+        return redirect(url)
+
+@login_required
+@doctor_required
+def delete_patient(request):
+    patient_id = request.GET.get('patient_id')
+    doctor_profile = get_object_or_404(DoctorProfile, user=request.user)
+    patient = get_object_or_404(Patients, doctor=doctor_profile, id=patient_id)
+
+    if request.method == 'POST': 
+        try:
+            name = patient.name
+            patient.delete()
+            messages.success(request, f"Patient {name} Delete successfully!")
+            return redirect("show_patients")
+        except Exception as e:
+            messages.error(request, f"Something went wrong while saving the new patient data: {str(e)}")
+            url = reverse('update_patient') + f'?patient_id={patient_id}'
+            return redirect(url)
+    else:
+        messages.error(request, "Method Not allowed")
+        return redirect("doctor_dashboard")
